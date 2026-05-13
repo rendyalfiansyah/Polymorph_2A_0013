61bef80eafe0b0a7a2b4c97721ca0374bd53a5c6
rendyalfiansyah <rendyalfiasyah356@gmail.com>
2026-05-13 10:26:44 +0700

    add proses transaksi dan perhitungan harga akhir

--- Kasir.java
+++ Kasir.java
@@ -9,4 +9,15 @@ public class Kasir {
         System.out.println("===== TOKO GARUDA =====");
         System.out.println("Nama Kasir: " + namaKasir);
     }
+
+    public void prosesBelanja(Produk p, int jmlhBeli) {
+
+        if (p.getStok() < jmlhBeli) {
+            System.out.println("Out of stok");
+        } else {
+            double hargaAkhir = p.hitungHargaAkhir(jmlhBeli);
+            System.out.println("Harga Akhir = " + hargaAkhir);
+            p.setStok(jmlhBeli);
+        }
+    }
 }
